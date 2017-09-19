039d73db8127ccadbd349061d58dc942704f5915
Vincent van Bergen <v.vanbergen@gmail.com>
2017-09-19 11:08:48 +0200

    refactoring data directory

--- scripts/run-lr.sh
+++ scripts/run-lr.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 advs="1458 2259 2261 2821 2997 3358 3386 3427 3476"
-folder=/data/vincent/make-ipinyou-data
+folder=/data/vincent/data
 for adv in $advs; do
     echo $adv
     python ../utilities/lryzx.py $folder/$adv/train.yzx.txt $folder/$adv/test.yzx.txt
--- scripts/run-rtb-test.sh
+++ scripts/run-rtb-test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 campaigns="1458 2259 2261 2821 2997 3358 3386 3427 3476"
-folder=/data/vincent/make-ipinyou-data
+folder=/data/vincent/data
 resultsfolder=../results
 
 if [ ! -d $resultsfolder ]; then
